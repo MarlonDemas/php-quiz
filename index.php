a283cd7fbeb51fab56bfa9d1e144098658ce60ee
MarlonDemas <demasmarlon@gmail.com>
2018-11-19 11:42:25 +0200

    Added external stylesheet and background image

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
      <title>Ultimate Soccer Quiz</title>
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
+     <link rel="stylesheet" href="css/main.css">
  </head>
  <body>
-     
+     <div class="container">
+     </div>
  </body>
  </html>
