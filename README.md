e968dd2adddfb177636685bbe16480915b8aa88f
MarlonDemas <demasmarlon@gmail.com>
2018-11-23 16:30:32 +0200

    Updated the readme and also added a retake button on the results page.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# php-quiz
-PHP Project 1 - Quiz
+#Ultimate Soccer Quiz
+## Description
+```
+The "Ultimate Soccer Quiz" is a php based quiz that tests your soccer knowledge. Most of the questions are basic just to see whether you if you know anything about the biggest and greatest sport in the world.
+```
+
+## Setting up my workspace
+
+- Created Repository
+- Opened terminal and launched the command 'git clone "repo link"'
+- Created base folder structure (sass, css, and images)
+- Pushed Changes
+
+## Learning
+```
+In this project I learned how to use foreach loops in php to make a form.
+
+I also learned how combine php, html and css to make the site look nice.
+
+Something new that I also learned was to make a fireworks effect using only css.
+```
+
+## ICT
+
+|**Technologies**|**Languages**|**Tools**|
+|:-----------:|:------------:|:------------:|
+| PHP Arrays | PHP | VScode
+| PHP Foreach loops | HTML | Firefox - Developer Edition
+| PHP Sessions |CSS (SASS) | Terminal
+| SASS        | | Github
+
+
+## Contributors
+Marlon Demas
--- results.php
+++ results.php
@@ -1,149 +1,121 @@
 <?php session_start(); ?>
  
- <!DOCTYPE html>
- <html>
- <head>
-     <meta charset="utf-8" />
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <title>Results</title>
-     <meta name="viewport" content="width=device-width, initial-scale=1">
-     <link rel="stylesheet" href="css/results.css">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
- </head>
- <body>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Results</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/results.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
+</head>
+<body>
 
-     <div>
-         
-     </div>
-     <?php
-        $questions = $_SESSION['questions'];
-        $answers = $_POST['answers'];
+    <!-- Making an array for the questions and answers -->
+    <?php
+    $questions = $_SESSION['questions'];
+    $answers = $_POST['answers'];
 
-        $counter = 0;
+    $counter = 0;
 
-        foreach ($questions as $questionNo => $value){
+    // Comparing the user answer and the correct answer
+    foreach ($questions as $questionNo => $value){
 
-            if ($answers[$questionNo] != $value['correctAnswer']){
-                // echo "$answers[$questionNo] was wrong";
-            } else {
-                $counter++;
-            }
-
+        if ($answers[$questionNo] != $value['correctAnswer']){
+            // echo "$answers[$questionNo] was wrong";
+        } else {
+            $counter++;
         }
-        
-     ?>
 
-     <?php if ($counter == 0) { 
-         $percent = ($counter/20)*100;
-     ?>
-        <div class="container">
-            <nav class="level">
-                <div class="level-item has-text-centered">
-                    <figure class='image'><img src='images\bomb.jpg'></figure>
-                </div>
-            </nav>
-            <audio controls autoplay class="is-invisible">
-                <source src="audio\Crowd Booing Sound Effects.mp3">
-            </audio>
-            <article class="message is-danger">
-                <div class="message-header">
-                    Congrats
-                </div>
-                <div class="message-body">
-                    <p class="title is-4">You got <?php echo $counter ?> out of 20 which is a solid </p>
-                    <p class="title is-3"><?php echo $percent ?>%</p>
-                </div>
-            </article>
-        </div>
-     <?php } ?>
+    }
+    
+    ?>
 
-     <?php if ($counter > 0 && $counter < 10) { 
-         $percent = ($counter/20)*100;
-     ?>
-        <div class="container">
-            <nav class="level">
-                <div class="level-item has-text-centered">
-                    <figure class='image'><img src='images/images.jfif'></figure>
-                </div>
-            </nav>
-            <audio controls autoplay class="is-invisible">
-                <source src="audio\Crowd Cheering - sound effect.mp3">
-            </audio>
-            <article class="message is-danger">
-                <div class="message-header">
-                    Congrats
-                </div>
-                <div class="message-body">
-                    <p class="title is-4">You got <?php echo $counter ?> out of 20 which is a solid </p>
-                    <p class="title is-3"><?php echo $percent ?>%</p>
-                </div>
-            </article>
-        </div>
-     <?php } ?>
-
-     <?php if ($counter > 10 && $counter < 15) { 
-         $percent = ($counter/20)*100;
-     ?>
-        <div class="container">
-            <nav class="level">
-                <div class="level-item has-text-centered">
-                    <figure class='image'><img src='images\what-if-i-told-you-its-not-good-enough.jpg'></figure>
-                </div>
-            </nav>
-            <audio controls autoplay class="is-invisible">
-                <source src="audio\Applause Crowd Cheering sound effect.mp3">
-            </audio>
-            <article class="message is-warning">
-                <div class="message-header">
-                    Congrats
-                </div>
-                <div class="message-body">
-                    <p class="title is-4">You got <?php echo $counter ?> out of 20 which is a solid </p>
-                    <p class="title is-3"><?php echo $percent ?>%</p>
-                </div>
-            </article>
-        </div>
-     <?php } ?>
-
-     <?php if ($counter > 15 && $counter< 20) { 
-         $percent = ($counter/20)*100; 
-     ?>
-        <div class="container">
+    <!-- Checks  -->
+    <?php if ($counter == 0) { 
+        $percent = ($counter/20)*100;
+    ?>
+    <div class="container">
         <nav class="level">
-          <div class="level-item has-text-centered">
-            <figure class='image'><img src='images/8vGX.gif'></figure>
-          </div>
-        </nav>
-            <audio controls autoplay class="is-invisible">
-                <source src="audio\3-1-10031.mp3">
-            </audio>
-            <article class="message is-success">
-                <div class="message-header">
-                    Congrats
-                </div>
-                <div class="message-body">
-                    <p class="title is-4">You got <?php echo $counter ?> out of 20 which is a solid </p>
-                    <p class="title is-3"><?php echo $percent ?>%</p>
-                </div>
-            </article>
-        </div>
-    <?php 
-        } 
-     ?>
-     <?php if ($counter == 20) {
-         $percent = ($counter/20)*100;
-     ?>
-        <div class="pyro">
-            <div class="before"></div>
-            <div class="after"></div>
-        </div>
-        <nav class="level">
-          <div class="level-item has-text-centered">
-            <figure class='image'><img src='images/LnrV.gif'></figure>
-          </div>
+            <div class="level-item has-text-centered">
+                <figure class='image'><img src='images\bomb.jpg'></figure>
+            </div>
         </nav>
         <audio controls autoplay class="is-invisible">
-                <source src="audio/UEFA Champions League 2016 2017 Theme Song AudioLyrics]-[AudioTrimmer.com].mp3">
+            <source src="audio\Crowd Booing Sound Effects.mp3">
+        </audio>
+        <article class="message is-danger">
+            <div class="message-header">
+                Congrats
+            </div>
+            <div class="message-body">
+                <p class="title is-4">You got <?php echo $counter ?> out of 20 which is a solid </p>
+                <p class="title is-3"><?php echo $percent ?>%</p>
+            </div>
+        </article>
+    </div>
+    <?php } ?>
+
+    <?php if ($counter > 0 && $counter < 10) { 
+        $percent = ($counter/20)*100;
+    ?>
+    <div class="container">
+        <nav class="level">
+            <div class="level-item has-text-centered">
+                <figure class='image'><img src='images/images.jfif'></figure>
+            </div>
+        </nav>
+        <audio controls autoplay class="is-invisible">
+            <source src="audio\Crowd Cheering - sound effect.mp3">
+        </audio>
+        <article class="message is-danger">
+            <div class="message-header">
+                Congrats
+            </div>
+            <div class="message-body">
+                <p class="title is-4">You got <?php echo $counter ?> out of 20 which is a solid </p>
+                <p class="title is-3"><?php echo $percent ?>%</p>
+            </div>
+        </article>
+    </div>
+    <?php } ?>
+
+    <?php if ($counter > 10 && $counter < 15) { 
+        $percent = ($counter/20)*100;
+    ?>
+    <div class="container">
+        <nav class="level">
+            <div class="level-item has-text-centered">
+                <figure class='image'><img src='images\what-if-i-told-you-its-not-good-enough.jpg'></figure>
+            </div>
+        </nav>
+        <audio controls autoplay class="is-invisible">
+            <source src="audio\Applause Crowd Cheering sound effect.mp3">
+        </audio>
+        <article class="message is-warning">
+            <div class="message-header">
+                Congrats
+            </div>
+            <div class="message-body">
+                <p class="title is-4">You got <?php echo $counter ?> out of 20 which is a solid </p>
+                <p class="title is-3"><?php echo $percent ?>%</p>
+            </div>
+        </article>
+    </div>
+    <?php } ?>
+
+    <?php if ($counter > 15 && $counter< 20) { 
+        $percent = ($counter/20)*100; 
+    ?>
+    <div class="container">
+    <nav class="level">
+        <div class="level-item has-text-centered">
+        <figure class='image'><img src='images/8vGX.gif'></figure>
+        </div>
+    </nav>
+        <audio controls autoplay class="is-invisible">
+            <source src="audio\3-1-10031.mp3">
         </audio>
         <article class="message is-success">
             <div class="message-header">
@@ -154,13 +126,41 @@
                 <p class="title is-3"><?php echo $percent ?>%</p>
             </div>
         </article>
-    <?php    
-        }
+    </div>
+<?php 
+    } 
     ?>
+    <?php if ($counter == 20) {
+        $percent = ($counter/20)*100;
+    ?>
+    <div class="pyro">
+        <div class="before"></div>
+        <div class="after"></div>
+    </div>
+    <nav class="level">
+        <div class="level-item has-text-centered">
+        <figure class='image'><img src='images/LnrV.gif'></figure>
+        </div>
+    </nav>
+    <audio controls autoplay class="is-invisible">
+            <source src="audio/UEFA Champions League 2016 2017 Theme Song AudioLyrics]-[AudioTrimmer.com].mp3">
+    </audio>
+    <article class="message is-success">
+        <div class="message-header">
+            Congrats
+        </div>
+        <div class="message-body">
+            <p class="title is-4">You got <?php echo $counter ?> out of 20 which is a solid </p>
+            <p class="title is-3"><?php echo $percent ?>%</p>
+        </div>
+    </article>
+<?php    
+    }
+?>
 
-    <center>
-        <a href="index.php" class="button is-light">Retake Quiz</a>
-    </center>
+<center>
+    <a href="index.php" class="button is-light">Retake Quiz</a>
+</center>
 
- </body>
- </html>
+</body>
+</html>
